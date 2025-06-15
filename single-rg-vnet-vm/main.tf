35a9b531f17b3904b0e65def517382370705a1cd
Bruno Brito <brunobritorj@outlook.com>
2025-06-15 12:41:41 +0200

    single-rg-vnet-vm: Add support to domain label

--- single-rg-vnet-vm/main.tf
+++ single-rg-vnet-vm/main.tf
@@ -48,6 +48,7 @@ resource "azurerm_public_ip" "main" {
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
   allocation_method   = "Static"
+  domain_name_label   = "${var.az_vm_name}-extip-1"
 }
 
 resource "azurerm_linux_virtual_machine" "main" {
